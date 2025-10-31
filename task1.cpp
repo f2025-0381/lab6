338f252196d12dca232f8cac14a906ae0540685d
f2025-0381 <f2025-0381@bnu.edu.pk>
2025-10-31 12:22:12 +0500

    Update task1.cpp

--- task1.cpp
+++ task1.cpp
@@ -3,16 +3,30 @@ using namespace std;
 
 int main() {
     int a, b, c;
-
     cout << "Enter three numbers: ";
     cin >> a >> b >> c;
 
-    if ((a > b && a < c) || (a < b && a > c))
-        cout << "The middle number is: " << a;
-    else if ((b > a && b < c) || (b < a && b > c))
-        cout << "The middle number is: " << b;
-    else
-        cout << "The middle number is: " << c;
+    cout << "Numbers in ascending order: ";
 
+    if (a <= b && a <= c) {
+        if (b <= c)
+            cout << a << ", " << b << ", " << c;
+        else
+            cout << a << ", " << c << ", " << b;
+    }
+    else if (b <= a && b <= c) {
+        if (a <= c)
+            cout << b << ", " << a << ", " << c;
+        else
+            cout << b << ", " << c << ", " << a;
+    }
+    else {
+        if (a <= b)
+            cout << c << ", " << a << ", " << b;
+        else
+            cout << c << ", " << b << ", " << a;
+    }
+
+    cout << endl;
     return 0;
 }
